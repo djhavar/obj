af4a41cd9815f185deb8823972c679e1937879c5
djhavar <90192071+djhavar@users.noreply.github.com>
2022-08-15 16:57:00 +0530

    Add files via upload

---
+++ obj.py
@@ -0,0 +1,42 @@
+class India():
+    def capital(self):
+        print("New Dheli is the capital of India")
+        def language(self):
+            print("hindi is the most widly spoken language in India")
+            def type(self):
+                print("India is a developing country")
+def Brazil():
+    def capital(self):
+                print("Brasília is the capital of Brazil")
+                def language(self):
+                    print("portugese is the most widly spoken language in Brazil")
+                    def type(self):
+                        print("Brazil is a developing country")
+                        
+class Japan():
+                    def capital(self):
+                        print("Tokyo is the capital of Japan")
+                        
+                        def language(self):
+                            print("japanese is the most widly spoken language in Japan")
+                            def type(self):
+                                print("Japan is a developed country")
+
+class Australia():
+    def capital(self):
+        print("Canberra is the capital of Australia")
+        def language(self):
+            print("english is the most widly spoken language in Australia")
+            def type(self):
+                print("Australia is a developed country")
+                
+obj_ind = India()
+obj_bra = Brazil()
+obj_jap = Japan()
+obj_aus = Australia()
+
+for country in (obj_ind, obj_bra, obj_jap, obj_aus):
+    
+ country.capital()
+country.language()
+country.type()
